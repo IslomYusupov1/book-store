256165186d1b374c293efc1d2a4e90aa0a24516f
islom0703 <66205811+islom0703@users.noreply.github.com>
2020-08-29 13:59:29 +0500

    price changed 132

--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -5,28 +5,28 @@ export default class BookstoreService {
             id: 1,
             title: 'Learning React',
             author: 'Banks Alex, Eve Porcello',
-            price: 60,
+            price: 100,
             coverImage: "https://m.media-amazon.com/images/I/51Q43WRXJzL.jpg"
         },
         {
             id: 2,
             title: 'Fullstack React',
             author: 'Ari Lerner, Nate Murray ',
-            price: 90,
+            price: 300,
             coverImage: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSqYX5QEZwHefA6KImnRE89ClJhgPxNqoFRKpBOgL4JCLLRAd36"
         },
         {
             id: 3,
             title: 'HTML & CSS',
             author: 'Jhon Jacket',
-            price: 30,
+            price: 200,
             coverImage: "https://litportal.ru/covers/38272898.jpg"
         },
         {
             id: 4,
             title: 'Изучаем JavaScript',
             author: 'Elizabet Robson',
-            price: 40,
+            price: 600,
             coverImage: "https://monster-book.com/sites/default/files/styles/sc290x400/public/books/izuchaem-programmirovanie-na-javascript.jpg?itok=4LNq8IGY"
         },
         {
